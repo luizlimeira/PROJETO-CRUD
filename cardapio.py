a2af5daaec19c7c86cdf4be7e45fcee4ed19e103
Rodrigo <rodrigo.lopessouza@hotmail.com>
2025-05-25 12:14:34 -0300

    criação atualizar

--- cardapio.py
+++ cardapio.py
@@ -15,3 +15,30 @@ def carregar_cardapio():
 def salvar_cardapio(cardapio):
     with open(ARQUIVO_CARDAPIO, 'w', encoding='utf-8') as f:
         json.dump(cardapio, f, indent=4, ensure_ascii=False)
+
+
+
+def atualizar_item():
+    cardapio = carregar_cardapio()
+    id_item = int(input("Informe o ID do item que deseja atualizar: "))
+    for item in cardapio:
+        if item['id'] == id_item:
+            print(">>>Deixei em branco para manter o valor atual<<<")
+            nome = input(f"Novo nome {item['nome']}: ") or item['nome']
+            descricao = input(f"Nova descrição ({item['descricao']}): ") or item['descricao']
+            ingredientes = input(f"Novo ingrediente ({item['ingredientes']}): ") or item['ingredientes']
+            preco_input = input(f"Novo preço ({item['preco']}): ")
+            preco = float(preco_input) if preco_input else item['preco']
+            categoria = input(f"Nova categoria ({item['categoria']}): ") or item['categoria']
+
+            item.update({
+                'nome': nome,
+                'descricao': descricao,
+                'ingredientes': ingredientes,
+                'preco': preco,
+                'categoria': categoria
+            })
+            salvar_cardapio(cardapio)
+            print("Item atualizado com sucesso!")
+            return
+    print("Item não encontrado.")
