bed9c17cc2505b3e4bc6e4030dbc06dfcfb58550
Rodrigo <rodrigo.lopessouza@hotmail.com>
2025-05-26 22:28:52 -0300

    desativação de funcções no main para testes

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 from cardapio import *
-from ficha import *
+# from ficha import *
 
-def menu_cardapio():
-    while True:
-        print("\n--- PEDIDOS ---")
-        print("1. Adicionar Pedido")
-        print("2. Listar Pedido")
-        print("3. Atualizar Pedido")
-        print("4. Remover Pedido")
-        print("0. Voltar")
-        op = input("Escolha: ")
+# def menu_pedido():
+#     while True:
+#         print("\n--- PEDIDOS ---")
+#         print("1. Adicionar Pedido")
+#         print("2. Listar Pedido")
+#         print("3. Atualizar Pedido")
+#         print("4. Remover Pedido")
+#         print("0. Voltar")
+#         op = input("Escolha: ")
 
-        if op == "1":
-            listar_cardapio()
-        elif op == "2":
-            adicionar_item()
-        elif op == "3":
-            atualizar_item()
-        elif op == "4":
-            remover_item()
-        elif op == "5":
-            filtrar_por_categoria()
-        elif op == "0":
-            break    
-        else:
-            print("Opção inválida.")
+#         if op == "1":
+#             # listar_pedidos()
+#         elif op == "2":
+#             adicionar_item()
+#         elif op == "3":
+#             atualizar_item()
+#         elif op == "4":
+#             remover_item()
+#         elif op == "5":
+#             filtrar_por_categoria()
+#         elif op == "0":
+#             break    
+#         else:
+#             print("Opção inválida.")
 
-def menu_fichas():
-    while True:
-        fichas = carregar_fichas()
+# def menu_fichas():
+#     while True:
+#         fichas = carregar_fichas()
         
-        print("\n--- MENU FICHAS ---")
-        print("1. Adicionar Ficha")
-        print("2. Visualizar Fichas")
-        print("3. Editar Ficha")
-        print("4. Excluir Ficha")
-        print("0. Voltar")
+#         print("\n--- MENU FICHAS ---")
+#         print("1. Adicionar Ficha")
+#         print("2. Visualizar Fichas")
+#         print("3. Editar Ficha")
+#         print("4. Excluir Ficha")
+#         print("0. Voltar")
         
-        opcao = input("Escolha uma opção: ")
+#         opcao = input("Escolha uma opção: ")
         
-        if opcao == "1":
-            fichas = adicionar_ficha(fichas)
-        elif opcao == "2":
-            visualizar_fichas(fichas)
-        elif opcao == "3":
-            editar_fichas(fichas)
-        elif opcao == "4":
-            excluir_fichas(fichas)
-        elif opcao == "0":
-            return
-        else:
-            print("\nOpção inválida!")
+#         if opcao == "1":
+#             fichas = adicionar_ficha(fichas)
+#         elif opcao == "2":
+#             visualizar_fichas(fichas)
+#         elif opcao == "3":
+#             editar_fichas(fichas)
+#         elif opcao == "4":
+#             excluir_fichas(fichas)
+#         elif opcao == "0":
+#             return
+#         else:
+#             print("\nOpção inválida!")
 
 def menu_cardapio():
     while True:
